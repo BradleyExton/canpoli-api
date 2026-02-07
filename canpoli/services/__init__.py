3e9daf09de4a4b68d7182025277e66c109c7a9d0
BradleyExton <bradley@bradleyexton.ca>
2026-02-07 09:13:58 -0500

    fix: remove missing ingestion service import

--- canpoli/services/__init__.py
+++ canpoli/services/__init__.py
@@ -3,11 +3,8 @@
 from canpoli.services.api_key_service import ApiKeyService
 from canpoli.services.billing_service import BillingService
 from canpoli.services.hoc_ingestion import HoCIngestionService
-from canpoli.services.hoc_parliament_ingestion import HoCParliamentIngestionService
-
 __all__ = [
     "ApiKeyService",
     "BillingService",
     "HoCIngestionService",
-    "HoCParliamentIngestionService",
 ]
